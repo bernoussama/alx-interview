92672399c9aec364dd78a7d38076f4a045eac3de
bernoussama <oussama.bernou@outlook.com>
2024-10-04 00:13:49 +0100

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [[1]]
+    triangle = [[1]]
+    for i in range(n - 1):
+        tmp = [0] + triangle[-1] + [0]
+        row = []
+        for j in range(len(triangle[-1]) + 1):
+            row.append(tmp[j] + tmp[j + 1])
+        triangle.append(row)
+    return triangle
