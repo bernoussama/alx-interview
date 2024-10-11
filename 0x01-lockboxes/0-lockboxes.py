fa45a7f2b1d45ba5d928b59b3791fdcd2dfd9625
bernoussama <oussama.bernou@outlook.com>
2024-10-11 01:27:59 +0100

    solution with types

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+can all boxes be unlocked ?
+"""
+
+
+def canUnlockAll(boxes: list[list[int]]) -> bool:
+    """
+    can all boxes be unlocked ?
+    """
+    n = len(boxes)
+    # list keeping track of unlocked boxes
+    unlocked = [False] * n
+    unlocked[0] = True
+
+    queue = [0]
+    while queue:
+        current = queue.pop(0)
+
+        for key in boxes[current]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                queue.append(key)
+
+    return all(unlocked)
