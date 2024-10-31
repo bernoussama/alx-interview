301561152fc46b4ba0114a54e084e7807ab52c0a
bernoussama <oussama.bernou@outlook.com>
2024-10-31 20:55:48 +0100

    utf8 validated succesfully

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,27 +11,37 @@ def validUTF8(data: List[int]):
     """
     Validate UTF-8 encoding
     """
-    valid = False
-    # print(format(0b10000000 >> 6, "08b"))
+
+    def countOnes(byt: int):
+        count = 0
+        for b in range(7, -1, -1):
+            if byt & (1 << b):
+                count += 1
+            else:
+                break
+        return count
+
     idx = 0
     while idx < len(data):
         byt = data[idx]
-        if byt >> 6 == 0b10:
-            return valid
-        valid = True
+        count = 0
 
+        count = countOnes(byt)
         # For a 1-byte character, the first bit is a 0, followed by its Unicode code.
-        if byt >> 7 == 0b0:
+        if count == 0:
+            idx += 1
             continue
+
+        if count == 1 or count > 4:
+            return False
 
         # For an n-bytes character,
         # the first n bits are all one's,
         # the n + 1 bit is 0,
-        if byt >> 6 == 0b11:
-            if byt >> 5 == 0b110:
-                if data[idx + 1] >> 6 == 0b10:
-                    valid = True
-                    idx = idx + 2
-            else:
-                return valid
-        # followed by n - 1 bytes with the most significant 2 bits being 10.
+        for n in range(idx + 1, idx + count):
+            # followed by n - 1 bytes with the most significant 2 bits being 10.
+            if countOnes(data[n]) != 1:
+                return False
+
+        idx += count - 1
+    return True
