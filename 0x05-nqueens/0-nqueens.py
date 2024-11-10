21595bb93f5a0ddb3e55d520b0018dd784b1a067
bernoussama <oussama.bernou@outlook.com>
2024-11-11 00:57:39 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,45 +1,89 @@
 #!/usr/bin/python3
 
 import sys
-from typing import List
 
 
-def nqueens(n) -> List[List[int]]:
-    """N queens"""
-    col = set()
-    pos_diag = set()
-    neg_diag = set()
-    res = []
-    board = [["."] * n for _ in range(n)]
-    answer = []
+def is_safe(board, row, col, n):
+    """
+    Check if a queen can be placed on board[row][col]
+    """
+    # Check this row on left side
+    for j in range(col):
+        if board[row][j] == 1:
+            return False
 
-    def backktrack(row):
-        positions = []
-        if row == n:
-            res.append(["".join(row) for row in board])
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens(n):
+    """
+    Solve the N queens problem and print all solutions
+    """
+    # Initialize the board
+    board = [[0 for x in range(n)] for y in range(n)]
+    solutions = []
+
+    def solve_util(board, col):
+        # Base case: If all queens are placed, return True
+        if col >= n:
+            solution = []
+            for i in range(n):
+                for j in range(n):
+                    if board[i][j] == 1:
+                        solution.append([i, j])
+            solutions.append(solution)
             return
+
+        # Consider this column and try placing this queen in all rows one by one
         for i in range(n):
-            if i in col or row + i in pos_diag or row - i in neg_diag:
-                continue
+            if is_safe(board, i, col, n):
+                # Place this queen in board[i][col]
+                board[i][col] = 1
 
-            col.add(i)
-            pos_diag.add(row + i)
-            neg_diag.add(row - i)
-            board[row][i] = "Q"
-            positions.append([row, i])
-            backktrack(row + 1)
-            col.remove(i)
-            pos_diag.remove(row + i)
-            neg_diag.remove(row - i)
-            board[row][i] = "."
-        answer.append(positions) if positions else None
+                # Recur to place rest of the queens
+                solve_util(board, col + 1)
 
-    backktrack(0)
-    return answer
+                # If placing queen in board[i][col] doesn't lead to a solution,
+                # then remove queen from board[i][col]
+                board[i][col] = 0
+
+    # Start from first column
+    solve_util(board, 0)
+
+    # Print all solutions
+    for solution in solutions:
+        print(solution)
+
+
+def main():
+    """Main function to handle input and run the program"""
+    # Check number of arguments
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve_nqueens(n)
 
 
 if __name__ == "__main__":
-    n = int(sys.argv[1])
-    res = nqueens(n)
-    for i in res:
-        print(i)
+    main()
