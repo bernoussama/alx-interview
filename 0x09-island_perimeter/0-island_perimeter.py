7492289d4c6d5605f9065ffe51fb2728314343f1
bernoussama <oussama.bernou@outlook.com>
2024-12-06 00:03:49 +0100

    simple himple

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,48 +7,19 @@ island perimeter
 def island_perimeter(grid):
     """island perimeter"""
 
-    def left(x, y):
-        return (x + 1, y)
+    perim = 0
+    rows, cols = len(grid), len(grid[0])
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perim += 4
+                if row < rows - 1 and grid[row + 1][col] == 1:
+                    perim -= 1
+                if col < cols - 1 and grid[row][col + 1] == 1:
+                    perim -= 1
+                if row > 0 and grid[row - 1][col] == 1:
+                    perim -= 1
+                if col > 0 and grid[row][col - 1] == 1:
+                    perim -= 1
 
-    def down(x, y):
-        return (x, y + 1)
-
-    def right(x, y):
-        return (x - 1, y)
-
-    def up(x, y):
-        return (x, y - 1)
-
-    def traverse(x, y, visited):
-        if (x, y) in visited:
-            return 0
-        visited.add((x, y))
-
-        perim = 0
-
-        # Check all four directions
-        for nx, ny in [left(x, y), right(x, y), up(x, y), down(x, y)]:
-            if (
-                nx < 0
-                or ny < 0
-                or nx >= len(grid)
-                or ny >= len(grid[0])
-                or grid[nx][ny] == 0
-            ):
-                perim += 1
-            elif grid[nx][ny] == 1:
-                perim += traverse(nx, ny, visited)
-
-        return perim
-
-    x, y = 0, 0
-    for idx, _ in enumerate(grid):
-        for j, n in enumerate(grid[idx]):
-            if n == 1:
-                x, y = idx, j
-                break
-        else:
-            continue  # only executed if the inner loop did NOT break
-        break
-
-    return traverse(x, y, set())
+    return perim
